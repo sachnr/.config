a5c26142fd52dd181bea5641ce6f6e6d7301bf4f
sachnr <sachinrawat544@gmail.com>
2025-08-14 00:03:45 +0530

    i[

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -1,30 +1,13 @@
 return {
-	"folke/snacks.nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		local Snacks = require("snacks")
+	{
+		"echasnovski/mini.nvim",
+		config = function()
+			require("mini.notify").setup({})
+		end,
+	},
 
-		Snacks.setup({
-			image = { enabled = true },
-			bigfile = { enabled = true },
-			zen = { enabled = true },
-			bufdelete = { enabled = true },
-			git = { enabled = true },
-			notifier = {
-				enabled = true,
-				timeout = 3000,
-			},
-		})
-
-		vim.keymap.set("n", "<M-z>", function()
-			Snacks.zen()
-		end, { silent = true, noremap = true })
-
-		vim.keymap.set("n", "<leader>gb", function()
-			Snacks.git.blame_line()
-		end, { silent = true, noremap = true })
-
-		vim.notify = Snacks.notifier.notify
-	end,
+	{
+		"folke/zen-mode.nvim",
+		opts = {},
+	},
 }
