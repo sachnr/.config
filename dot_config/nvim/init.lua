93fb36ed0eff56aa86687042297355276a2a4f20
sachnr <sachinrawat544@gmail.com>
2025-08-19 20:07:26 +0530

    neovide config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -103,17 +103,23 @@ require("cyberdream").setup({
 })
 vim.cmd.colorscheme("cyberdream")
 
--- if vim.g.neovide then
--- vim.o.guifont = "Iosevka Term:h14"
--- vim.g.neovide_text_gamma = 0.0
--- vim.g.neovide_text_contrast = 0
--- vim.g.neovide_position_animation_length = 0
--- vim.g.neovide_cursor_animation_length = 0
--- vim.g.neovide_cursor_trail_size = 0
--- vim.g.neovide_cursor_animate_in_insert_mode = false
--- vim.g.neovide_cursor_animate_command_line = false
--- vim.g.neovide_scroll_animation_far_lines = 0
--- vim.g.neovide_scroll_animation_length = 0
--- vim.g.neovide_refresh_rate = 180
--- vim.g.neovide_refresh_rate_idle = 5
--- end
+if vim.g.neovide then
+	vim.o.guifont = "Iosevka Curly Slab:h14"
+	vim.g.neovide_text_gamma = 0.0
+	vim.g.neovide_text_contrast = 0
+
+	vim.g.neovide_position_animation_length = 0.04
+	vim.g.neovide_cursor_animation_length = 0.05
+	vim.g.neovide_cursor_short_animation_length = 0.02
+	vim.g.neovide_scroll_animation_length = 0.08
+	vim.g.neovide_scroll_animation_far_lines = 2
+
+	vim.g.neovide_cursor_trail_size = 0
+
+	vim.g.neovide_cursor_animate_in_insert_mode = true
+	vim.g.neovide_cursor_animate_command_line = true
+	vim.g.neovide_cursor_antialiasing = true
+
+	vim.g.neovide_refresh_rate = 180
+	vim.g.neovide_refresh_rate_idle = 5
+end
